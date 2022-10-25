22523e1b111dbccff41316a1b38732e6f5c594cf
David Fuentes <dfuentes@ahumada.cl>
2022-10-25 08:43:27 -0300

    save high score to a file

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,7 @@ class Scoreboard(Turtle):
 	def __init__(self) -> None:
 		super().__init__()
 		self.score = 0
-		self.high_score = 0
+		self.high_score = self.get_high_score()
 		self.penup()
 		self.hideturtle()
 		self.color("white")
@@ -28,9 +28,21 @@ class Scoreboard(Turtle):
 	def reset(self):
 		if self.score > self.high_score:
 			self.high_score = self.score
+			self.set_high_score()
 		self.score = 0
 		self.update_scoreboard()
 
 	def game_over(self):
 		self.goto(0, 0)
 		self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+	@staticmethod
+	def get_high_score():
+		with open("score.txt", mode="r") as file:
+			score = file.read()
+
+		return int(score)
+
+	def set_high_score(self):
+		with open("score.txt", mode="w") as file:
+			file.write(str(self.high_score))
